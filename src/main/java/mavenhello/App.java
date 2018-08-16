97a8d763fc316c1dbc6dd2c26ce9b868d2b9211f
jtcheng <jtcheng@alauda.io>
2018-08-16 10:39:13 +0800

    add more bad code
    Signed-off-by: jtcheng <jtcheng@alauda.io>

--- src/main/java/mavenhello/App.java
+++ src/main/java/mavenhello/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        String abc = "111";
+        if (abc == "111"){
+            System.out.println("ooo");
+        }
         System.out.println( "Hello Alauda - Maven !" );
     }
 }
